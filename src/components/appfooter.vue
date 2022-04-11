e21cfa8aa6f0f398e8901c14dd29d388f77ee980
Mayur Chaudhari <mayur.chaudhari@ranium.in>
2022-04-11 10:00:15 +0530

    add some padding and margin into footer component

--- src/components/appfooter.vue
+++ src/components/appfooter.vue
@@ -1,16 +1,16 @@
 <template>
 <div class="footer  bg-blue">
-   <div class="container  mx-auto pt-20 pb-60">
+   <div class="container  mx-auto pt-28 pb-64">
     <div class="font-black text-center cursor-pointer text-3xl">
     <span class="text-orange">App</span>
     <span class="text-white">Lab</span>
    </div>
-   <div class="flex items-center flex-col justify-center lg:flex-row my-6 ">
-      <div class=" text-white text-sm font-semibold my-4 lg:mx-4 cursor-pointer">Home</div>
-       <div class=" text-white text-sm font-semibold my-4 lg:mx-4 cursor-pointer">Key Features</div>
-        <div class=" text-white text-sm font-semibold my-4 lg:mx-4 cursor-pointer">Pricing</div>
-         <div class=" text-white text-sm font-semibold my-4 lg:mx-4 cursor-pointer">Testiminal</div>
-          <div class=" text-white text-sm font-semibold my-4 lg:mx-4 cursor-pointer">FAQ</div>
+   <div class="flex items-center flex-col justify-center lg:flex-row my-12 ">
+      <div class=" text-white text-base my-4 lg:my-0 lg:mx-4 cursor-pointer">Home</div>
+       <div class=" text-white text-base my-4 lg:my-0 lg:mx-4 cursor-pointer">Key Features</div>
+        <div class=" text-white text-base my-4 lg:my-0 lg:mx-4 cursor-pointer">Pricing</div>
+         <div class=" text-white text-base my-4 lg:my-0 lg:mx-4 cursor-pointer">Testiminal</div>
+          <div class=" text-white text-base my-4 lg:my-0 lg:mx-4 cursor-pointer">FAQ</div>
    </div>
      <div class="flex items-center flex-row justify-center">
         <img src="../assets/socialicons/facebook-app-symbol.png" alt="facebook" class="w-8 h-8 mx-4 opacity-10 hover:opacity-100 cursor-pointer">
